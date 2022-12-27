b5be3d95b8f3fd07b1185ff6813aca4aed2daecb
Slothborg <SlothBorg@users.noreply.github.com>
2022-12-27 08:44:21 -0500

    wip

---
+++ app/Http/Livewire/History/Event/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\History\Event;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.history.event.create');
+    }
+}
